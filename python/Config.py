99c5e94024f8fe89811e5f94c78330f6f012168f
Eddie G <60665719+Garzlok@users.noreply.github.com>
2026-04-08 22:14:44 -0500

    Revert "April 6, 2026 Update"

--- python/Config.py
+++ python/Config.py
@@ -21,9 +21,6 @@ config['loadcell.debug' ] = False#True
 #logging settings for iSpindel Devices
 config['iSpindel.debug' ] = False#True
 
-#logging settings for the Websocket
-config['websocket.debug' ] = False#True
-
 #Use fake monitoring to report flow
 config['dispatch.debugMonitoring' ] = False
 
